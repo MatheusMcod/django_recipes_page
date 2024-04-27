389f3df754d929c692a413c55fefacc5a08ec368
MatheusMcod <matheus.moreiracosta300@gmail.com>
2024-04-27 14:24:58 -0300

    initial_structure

---
+++ PageRecipes/recipes/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return HttpResponse('ABOUT')
+
+def contact(request):
+    return HttpResponse('contact')
