248f2ad880ce15b42d48916cd5b57aca1249ceb6
MatheusMcod <matheus.moreiracosta300@gmail.com>
2024-04-28 09:10:53 -0300

    static_css

--- PageRecipes/recipes/views.py
+++ PageRecipes/recipes/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/pages/home.html')
 
 def about(request):
     return HttpResponse('ABOUT')
