23cfb4f2393d0408361ff322e32f0f051da5d12c
MatheusMcod <matheus.moreiracosta300@gmail.com>
2024-04-29 22:41:00 -0300

    creation_exibition_database

--- PageRecipes/recipes/views.py
+++ PageRecipes/recipes/views.py
@@ -1,11 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes
+    })
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
 
 def recipes(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipeView.html', context={
-        'is_detail_text': True
+        'recipe': recipe,
+        'is_detail_page': True,
     })
 
